d75fbe4e48154f5cbe15c05413ba9fec4dcdd4ca
Abdullah <157466888+rootL0r@users.noreply.github.com>
2024-04-24 00:15:42 -0700

    Add files via upload

---
+++ Csgo2HaCk/Program.cs
@@ -0,0 +1,134 @@
+﻿using Swed64;
+using System;
+using System.Numerics;
+using System.Runtime.InteropServices;
+
+
+class Program
+{
+    [DllImport("user32.dll")]
+
+
+    static extern short GetAsyncKeyState(Keys vKey);
+
+    // Main addr
+    static int Localplayer   = 0x017361C8;
+    static int entitylist7   = 0x01743C80;
+    static int viewangelsY   = 0x192F910;
+    static int viewangelsX   = 0x192F914;
+    static int viewangels    = 0x192F910;
+
+    // offsets
+
+    static int health       = 0x334;
+    static int teamnumber   = 0xEE8;
+    static int position     = 0xD60;
+
+    class Entity
+    {
+        public IntPtr address;
+        public int health;
+        public int teamnumber;
+        public Vector3 position;
+    }
+
+    static Swed swed = new Swed("cs2");
+    static Entity localplayer = new Entity();
+    static List<Entity> playerteam = new List<Entity>();
+    static List<Entity> enemyteam = new List<Entity>();
+
+    static IntPtr clientDLL = swed.GetModuleBase("client.dll");
+    static void Main(string[] args)
+    {
+        localplayer.address = swed.ReadPointer(clientDLL, Localplayer);
+
+
+        while (true)
+        {
+            Entitylist();
+
+
+            if (enemyteam.Count > 0)
+            {
+                aimBot(enemyteam[0]);
+            }
+                
+            
+           
+        }
+
+    }
+
+    static void updateEntity(Entity entity)
+    {
+        entity.health = swed.ReadInt(entity.address, health);
+        entity.teamnumber = swed.ReadInt(entity.address, teamnumber);
+        entity.position = swed.ReadVec(entity.address, position);
+
+    }
+
+    static void Entitylist()
+    {
+        playerteam.Clear();
+        enemyteam.Clear();
+
+        // Distance between each entity is 0x8
+        localplayer.address = swed.ReadPointer(clientDLL, Localplayer);
+        updateEntity(localplayer);
+
+        for (int i = 1; i < 32; i++)
+        {
+         Entity entity = new Entity();
+
+
+            entity.address = swed.ReadPointer(clientDLL, entitylist7 + i * 0x8);
+            updateEntity(entity);
+          
+            if (entity.health <= 0 || entity.health > 100)
+                      continue;
+
+            Console.WriteLine(entity.health);
+
+            if (localplayer.teamnumber == entity.teamnumber)
+            {
+                playerteam.Add(entity);
+            }
+            
+            else
+            {
+                enemyteam.Add(entity);
+            }
+        }
+
+
+    }
+
+    static void aimBot(Entity entity)
+    {
+        localplayer.address = swed.ReadPointer(clientDLL, Localplayer);
+        updateEntity(localplayer);
+
+        float deltaX = entity.position.X - localplayer.position.X;
+        float deltaY = entity.position.Y - localplayer.position.Y;
+        float deltaZ = entity.position.Z - localplayer.position.Z;
+
+        double distance = Math.Sqrt(Math.Pow(deltaY, 2) + Math.Pow(deltaX, 2));
+
+        float X = (float)(Math.Atan2(deltaY, deltaX) * 180 / Math.PI);
+        float Y = -(float)(Math.Atan2(deltaZ - 10f, distance) * 180 / Math.PI);
+
+        Vector3 Aimbot = new Vector3(Y, X, 0);
+
+        var view4angles = swed.ReadVec(clientDLL, viewangels);
+
+        var L = GetAsyncKeyState(Keys.L);
+
+
+        if (0 > L)
+        {
+            swed.WriteVec(clientDLL, viewangels, Aimbot);
+        }
+
+    }
+
+}
